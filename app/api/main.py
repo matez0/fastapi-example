d68b61e6a5cf8468e287c2a4b8bfda3dc57b0b9b
Zoltán Máté <mate.zoltan0@gmail.com>
2023-04-03 02:20:44 +0200

    Connect to and disconnect from database on startup and shutdown events

--- app/api/main.py
+++ app/api/main.py
@@ -2,6 +2,18 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
+from app.db.base import database
+
 api = FastAPI()
 
 not_found_response = {HTTPStatus.NOT_FOUND.value: {'model': None}}
+
+
+@api.on_event('startup')
+async def startup():
+    await database.connect()
+
+
+@api.on_event('shutdown')
+async def shutdown():
+    await database.disconnect()
